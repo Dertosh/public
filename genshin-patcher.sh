a54c041ebf7e111014ea06a384207de0720c649f
Dertosh <Dertosh7@gmail.com>
2021-06-13 03:08:22 +0300

    Update genshin-patcher.sh

--- genshin-patcher.sh
+++ genshin-patcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash env
 cd $GAMEDIR/drive_c/Program\ Files/Genshin\ Impact/Genshin\ Impact\ game/
-bash $CACHE/patch/151/patch.sh
-bash $CACHE/patch/151/patch_anti_logincrash.sh
+bash $CACHE/patch/161/patch.sh
+bash $CACHE/patch/161/patch_anti_logincrash.sh
